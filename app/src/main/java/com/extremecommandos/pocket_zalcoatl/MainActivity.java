c7ac4c67b17a7344db68f1b62254713e244a40ea
a-rmz <armzprz@gmail.com>
2016-03-08 09:07:32 -0600

    Stat bars with icons

--- app/src/main/java/com/extremecommandos/pocket_zalcoatl/MainActivity.java
+++ app/src/main/java/com/extremecommandos/pocket_zalcoatl/MainActivity.java
@@ -8,8 +8,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
-import com.extremecommandos.pocket_zalcoatl.R;
-
 import com.akexorcist.roundcornerprogressbar.*;
 
 /**
