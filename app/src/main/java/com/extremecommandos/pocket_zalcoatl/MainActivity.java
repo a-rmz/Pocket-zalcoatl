08187ef48d11bdd5a5b65d7d9dfb36246eaff13e
a-rmz <armzprz@gmail.com>
2016-03-08 07:43:24 -0600

    Failed changes

--- app/src/main/java/com/extremecommandos/pocket_zalcoatl/MainActivity.java
+++ app/src/main/java/com/extremecommandos/pocket_zalcoatl/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.support.design.widget.Snackbar;
 import android.widget.Button;
 
 import com.akexorcist.roundcornerprogressbar.*;
